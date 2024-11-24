facf5722f48d3883578e0db2db4320d16f695b25
John Kalyanapu <22058358+toofan1@users.noreply.github.com>
2024-11-23 19:27:58 -0500

    add Telugu Template Note and a shell script for linking media to build an anki deck

---
+++ oxford3000_telugu_anki/update_anki_links.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Paths
+GIT_MEDIA_FOLDER=~/Documents/git/telugu-kr-david-autobio/oxford3000_telugu_anki/media/
+ANKI_MEDIA_FOLDER=~/.local/share/Anki2/User\ 1/collection.media
+
+# Remove old links
+find "$ANKI_MEDIA_FOLDER" -type l -exec rm {} \;
+
+# Recreate links for all files in the Git media folder
+find "$GIT_MEDIA_FOLDER" -type f -exec ln -s {} "$ANKI_MEDIA_FOLDER/" \;
+
+# Print completion message
+echo "Media links updated!"
+
